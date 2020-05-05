b71aa74c94dec2ba952bd4f3d90a9266803d6147
bhaniel <bhaniel@gmail.com>
2020-05-05 08:45:21 +0000

    changed the headers to the new convension

--- src/controllers/home.ts
+++ src/controllers/home.ts
@@ -9,8 +9,8 @@ export const index = (req: Request, res: Response) => {
     console.log(counter++);
     // res.setHeader('Content-Type', 'text/html');
     res.header("Content-Type",'application/json');
-    res.setHeader('Accept-Ch' , 'UA, Platform, Arch, Model, Mobile') 
-    // res.setHeader('Accept-Ch-Lifetime' , '86400');
+    res.header('Accept-CH' , 'UA-Full-Version, UA-Platform, UA-Arch') 
+    //res.setHeader('Accept-Ch-Lifetime' , '86400');
     res.send(JSON.stringify({ test : "test" , headers : req.headers }, null, 4))
     
 };
